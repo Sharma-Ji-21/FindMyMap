ae4b628ae7916e0b77ff3e9bd9ebdbcdf32e4500
Om Sharma <justsharmaji69@gmail.com>
2025-07-27 18:33:10 +0530

    origin and destination added with a smooth animation

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MI-Flutter',
+      title: 'FindMyMap',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primaryColor: Colors.white),
       home: MapScreen(),
--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -15,9 +14,12 @@ class _MapScreenState extends State<MapScreen> {
     target: LatLng(28.613939, 77.209023),
     zoom: 11.5,
   );
+
   late GoogleMapController _googleMapController;
-  late double currentLatitude;
-  late double currentLongitude;
+  Marker? _origin;
+  Marker? _destination;
+  double? currentLatitude;
+  double? currentLongitude;
 
   @override
   void dispose() {
@@ -32,29 +34,132 @@ class _MapScreenState extends State<MapScreen> {
   }
 
   Future<void> getCurrentLocation() async {
+    bool serviceEnabled;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      print('Location services are disabled.');
+      return;
+    }
+
     try {
-      await Geolocator.requestPermission();
       Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high,
       );
-      currentLatitude = position.latitude;
-      currentLongitude = position.longitude;
+      setState(() {
+        currentLatitude = position.latitude;
+        currentLongitude = position.longitude;
+      });
     } catch (e) {
-      print(e);
+      print('Error getting location: $e');
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text("FindMyMap"),
+        actions: [
+          if (_origin != null)
+            TextButton(
+              onPressed: () => _googleMapController.animateCamera(
+                duration: const Duration(milliseconds: 1000),
+                CameraUpdate.newCameraPosition(
+                  CameraPosition(
+                    target: _origin!.position,
+                    zoom: 14.5,
+                    tilt: 50.0,
+                  ),
+                ),
+              ),
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.green,
+                textStyle: const TextStyle(fontWeight: FontWeight.w600),
+              ),
+              child: Text("Origin"),
+            ),
+          if (_destination != null)
+            TextButton(
+              onPressed: () => _googleMapController.animateCamera(
+                duration: const Duration(milliseconds: 1000),
+                CameraUpdate.newCameraPosition(
+                  CameraPosition(
+                    target: _destination!.position,
+                    zoom: 14.5,
+                    tilt: 50.0,
+                  ),
+                ),
+              ),
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.blue,
+                textStyle: TextStyle(fontWeight: FontWeight.w600),
+              ),
+              child: Text("Destination"),
+            ),
+        ],
+      ),
       body: GoogleMap(
         myLocationButtonEnabled: false,
-        zoomControlsEnabled: true,
+        myLocationEnabled: true,
+        zoomControlsEnabled: false,
         initialCameraPosition: _initialCameraPosition,
         onMapCreated: (controller) {
           _googleMapController = controller;
         },
+        markers: {
+          if (_origin != null) _origin!,
+          if (_destination != null) _destination!,
+        },
+        onLongPress: _addMarker,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (currentLatitude != null && currentLongitude != null) {
+            CameraPosition newCameraPosition = CameraPosition(
+              target: LatLng(currentLatitude!, currentLongitude!),
+              zoom: 18.5,
+              tilt: 50.0,
+            );
+            _googleMapController.animateCamera(
+              duration: const Duration(milliseconds: 1500),
+              CameraUpdate.newCameraPosition(newCameraPosition),
+            );
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text("Location not available yet")),
+            );
+          }
+        },
+        backgroundColor: Theme.of(context).primaryColor,
+        child: const Icon(Icons.center_focus_strong),
       ),
     );
+  }
+
+  void _addMarker(LatLng argument) {
+    if (_origin == null || (_origin != null && _destination != null)) {
+      setState(() {
+        _origin = Marker(
+          markerId: const MarkerId('origin'),
+          infoWindow: const InfoWindow(title: 'Origin'),
+          icon: BitmapDescriptor.defaultMarkerWithHue(
+            BitmapDescriptor.hueGreen,
+          ),
+          position: argument,
+        );
+        _destination = null;
+      });
+    } else {
+      setState(() {
+        _destination = Marker(
+          markerId: const MarkerId('destination'),
+          infoWindow: const InfoWindow(title: 'Destination'),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+          position: argument,
+        );
+      });
+    }
   }
 }
